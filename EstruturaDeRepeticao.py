cc6ea1f1812ab3ff7e61550c193ebfef348c6055
DaniHenriqueAOM <rm94615@fiap.com.br>
2022-04-12 11:15:09 -0300

    Add exemplo estrutura de repetição.

---
+++ EstruturaDeRepeticao.py
@@ -0,0 +1,6 @@
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
+
+print(f"Valor de x: {x}")
---
+++ EstruturaDeRepeticap2.py
@@ -0,0 +1,6 @@
+x = int(input("Valor: "))
+cont = 0
+while cont < 5:
+    x += 1
+    print("Cont: ", cont)
+    cont += 1
